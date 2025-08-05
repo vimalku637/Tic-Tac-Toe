3ede6419f99c55603594fef19524d1be07aa11c8
Vimal Kumar <vimalku637@gmail.com>
2025-08-05 13:39:50 +0530

    support to android 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sct.tictactoe"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
